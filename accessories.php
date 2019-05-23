db6021a01e07c6c7fc182bce1f4a6f3f6d861acf
renatolhernandez2017 <renatolhernandez@hotmail.com>
2019-05-23 19:25:42 -0300

    Abas do Menu

---
+++ accessories.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title> Petitau </title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="/index.css" type="text/css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script type="text/javascript" src="index.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+
+	<?php include '../header.php';?>
+
+	<div class="container-fluid text-center">
+		  <div class="row">
+
+	        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+	            <div class="card">
+	                <img class="card-body img-fluid" src="/imagens/journal.png" alt="Card image">
+	                <div class="card-body">
+	                    <h4 class="card-title"> Curiosidades </h4>
+	                    <h5 class="card-title"> Mistérios do Mundo Animal </h5>
+	                    <a href="#" class="btn btn-primary"> Saiba Mais </a>
+	                </div>
+	            </div>
+	        </div>
+
+	        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+	            <div class="card">
+	                <img class="card-body img-fluid" src="/imagens/medicine.png" alt="Card image">
+	                <div class="card-body">
+	                    <h4 class="card-title"> Saúde </h4>
+	                    <h5 class="card-title"> Conselhos de Beleza </h5>
+	                    <a href="#" class="btn btn-primary"> Saiba Mais </a>
+	                </div>
+	            </div>
+	        </div>
+
+	        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+	            <div class="card">
+	                <img class="card-body img-fluid" src="/imagens/heart.png" alt="Card image">
+	                <div class="card-body">
+	                    <h4 class="card-title"> Cuidados </h4>
+	                    <h5 class="card-title"> Cuidados básicos </h5>
+	                    <a href="#" class="btn btn-primary"> Saiba Mais </a>
+	                </div>
+	            </div>
+	        </div>
+
+	        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+	            <div class="card">
+	                <img class="card-body img-fluid" src="/imagens/alimento.png" alt="Card image">
+	                <div class="card-body">
+	                    <h4 class="card-title"> Alimentação </h4>
+	                    <h5 class="card-title"> Dietas & Problemas </h5>
+	                    <a href="#" class="btn btn-primary"> Saiba Mais </a>
+	                </div>
+	            </div>
+	        </div>
+
+	    </div>
+	</div>
+
+	<br>
+
+	<?php include '../footer.php';?>
+
+</body>
+</html>
